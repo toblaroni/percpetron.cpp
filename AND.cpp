4c1838fd57c3c970eda8731430543c48997f5fea
toblaroni <tobyhutchinson@gmail.com>
2024-01-20 17:02:34 +0000

    created and.cpp

---
+++ AND.cpp
@@ -0,0 +1,20 @@
+
+#include "perceptron.h"
+#include <stdio.h>
+
+// AND logic
+float and_training[4][2] = {
+   { 0.0, 0.0 },
+   { 0.1, 0.0 },
+   { 0.0, 0.1 },
+   { 0.1, 0.1 }
+};
+
+float and_training_ans[4] = { 0.0, 0.0, 0.0, 0.1 };
+
+int main(int argc, char **argv) {
+   
+
+
+   return 0;
+}
